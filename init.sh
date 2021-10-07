21a44bf573805e95708f45b4d4f4918a02fc2eb7
t4t5u0 <t4t5u0@enPiT2019MBA-02.local>
2021-10-07 14:47:49 +0900

    Update: copy font setting

--- init.sh
+++ init.sh
@@ -18,3 +18,21 @@ ln -sf $MY_PWD/dotfile/zsh/.zshenv $HOME/.zshenv
 ln -sf $MY_PWD/dotfile/zsh/.zsh $HOME/.zsh
 ln -sf $MY_PWD/dotfile/zsh/.dir_colors $HOME/.dir_colors
 
+
+mkdir font 
+cd font
+
+curl -JLOs https://github.com/miiton/Cica/releases/download/v5.0.2/Cica_v5.0.2_with_emoji.zip
+
+unzip -q ./*.zip
+
+if [ "$(uname)" == 'Darwin' ]; then
+  cp -f ./*.ttf ~/Library/Fonts
+fi
+
+fc-cache -vf
+
+cd ..
+rm -rf font
+
+echo "TerminalのfontをCicaに変更して下さい"
