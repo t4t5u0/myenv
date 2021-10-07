e500aa155b0c47591e272c3d16e4e0f406532ff7
t4t5u0 <t4t5u0@enPiT2019MBA-02.local>
2021-10-07 14:23:59 +0900

    Update

--- init.sh
+++ init.sh
@@ -1,3 +1,16 @@
-cp dotfile/zsh/* ~/ -rf && \
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | sh && \
-source ~/.zshrc
+MY_PWD="$(pwd)"
+
+if [ "$(uname)" == 'Dawrin' ]; then
+    brew install starship
+fi
+
+mkdir -p $HOME/.config 
+
+cd $HOME/.config
+ln -sf $MY_PWD/starship.toml starship.toml
+cd $MY_PWD
+
+ln -sf $MY_PWD/dotfile/zsh/.zshrc $HOME/.zshrc
+ln -sf $MY_PWD/dotfile/zsh/.zshenv $HOME/.zshenv
+ln -sf $MY_PWD/dotfile/zsh/.zsh $HOME/.zsh
+ln -sf $MY_PWD/dotfile/zsh/.dir_colors $HOME/.dir_colors
